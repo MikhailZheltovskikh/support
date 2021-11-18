c299f2c695a782e88a58acb33713c7713a4ac449
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2021-11-18 13:53:50 +0500

    added page inhex.html

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+$('.questions__slider').slick({
+   nextArrow: '<button class="slick-next slick-next"><img src="images/next.svg" alt="next arrow"></button>', 
+   slidesToShow: 4,
+   slidesToScroll: 1,  
+   focusOnSelect: true,    
+   arrows: true,
+   // responsive: [
+   //     {
+   //       breakpoint: 960,
+   //       settings: {
+   //       slidesToShow: 5,
+ 
+   //       }
+   //     },
+   //     {
+   //       breakpoint: 750,
+   //       settings: {
+   //       slidesToShow: 4,
+ 
+   //       }
+   //     },
+   //     {
+   //       breakpoint: 650,
+   //       settings: {
+   //       slidesToShow: 3,
+   //       }
+   //     },
+       
+   // ]  
+ 
+ 
+ });
